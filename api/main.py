c082e7da80a041592348b9431c40976dc544c995
retepicheep <peter.dillow@gmail.com>
2022-07-30 12:27:43 -0700

    edited virtualenv

--- api/main.py
+++ api/main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def boring():
-  return "I already did this!"
+@app.route("/new-image")
+def new_image():
+  word = request.args.get("query")
+  return {"word": word}
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5050)
