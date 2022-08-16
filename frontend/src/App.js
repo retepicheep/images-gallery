329114d313dffd9a5dbfd790bed37b73ae1705bd
retepicheep <peter.dillow@cchsnw.org>
2022-08-16 14:29:54 -0700

    feat: get saved images in the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
@@ -13,6 +13,18 @@ const API_URL = process.env.REACT_APP_API_URL || "http://ringil.local:5050";
 const App = () => {
   const [word, setWord] = useState("");
   const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    async function getSavedImages() {
+      try {
+        const res = await axios.get(`${API_URL}/images`);
+        setImages(res.data || []);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getSavedImages();
+  }, [word]);
 
   const handleSearchSubmit = async (e) => {
     e.preventDefault();
